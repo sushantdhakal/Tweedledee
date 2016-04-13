a85c772bb1b77c2fbf5d45d8265af1fcb2d0c081
psmichalek <mich0044@umn.edu>
2016-04-12 23:13:26 -0500

    login tests completed; other tests stubbed out

--- src/integration-test/groovy/tweedledee/LoginPageFunctionalSpec.groovy
+++ src/integration-test/groovy/tweedledee/LoginPageFunctionalSpec.groovy
@@ -6,19 +6,44 @@ import grails.test.mixin.integration.Integration
 @Integration
 class LoginPageFunctionalSpec extends GebSpec {
 	
+	// Requirment: L1
 	def 'User is routed to login page when not logged in'(){
 		when:
 	    go '/'
 
 	    then:
-	    $('h1').first().text()=='TweedleDee'
+	    waitFor("quick"){ $("#username") && $("#password") }
 	}
 
+	// Requirment: L3
 	def 'Invalid login is rejected with an error message'(){
 		when:
 		go '/#/login'
+		waitFor("quick"){ 
+			$("#username").value("abc")
+			$("#password").value("123456789")
+			$("#submitBtn").click();
+		}
 
 		then:
-		waitFor("quick"){ $("#username") }
+		waitFor(){
+			$("#errorMessage",text:contains("Invalid login"))
+		}
+	}
+
+	// Requirment: L2
+	def 'Login screen allows a user to enter username and password to gain access'(){
+		when:
+		go '/#/login'
+		waitFor("quick"){ 
+			$("#username").value("admin")
+			$("#password").value("12345678pP")
+			$("#submitBtn").click();
+		}
+
+		then:
+		waitFor(){
+			currentUrl.contains "profile"
+		}
 	}
 }
---
+++ src/integration-test/groovy/tweedledee/UserDetailFunctionalSpec.groovy
@@ -0,0 +1,9 @@
+package tweedledee
+
+import geb.spock.GebSpec
+import grails.test.mixin.integration.Integration
+
+@Integration
+class UserDetailFunctionalSpec extends GebSpec {
+
+}
